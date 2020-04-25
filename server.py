5fd6b1986ab99bfbf4964ee1a3e04f3b79066326
Graham Miller <grahamiller96@gmail.com>
2020-04-25 13:56:15 -0400

    cowboy push

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ class Battlesnake(object):
         # cherrypy.request.json contains information about the game that"s about to be played.
         # TODO: Use this function to decide how your snake is going to look on the board.
         data = cherrypy.request.json
-        return {"color": "#565656", "headType": "shac-workout", "tailType": "round-bum"}
+        return {"color": "#565656", "headType": "shac-tiger-king", "tailType": "round-bum"}
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
